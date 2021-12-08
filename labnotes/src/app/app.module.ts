07c82a6e2660321672f96537ad7e5eaf3a133cfa
ezritz <lidia.edith.escoto@gmail.com>
2021-12-07 23:42:02 -0600

    Create login component

--- labnotes/src/app/app.module.ts
+++ labnotes/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ labnotes/src/app/components/log-in/log-in.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector:'app-log-in',
+    templateUrl: './log-in.component.html',
+    styleUrls: ['./log-in.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+
+export class LogInComponent {
+   
+}
